616123949f90357ab862e2052a3a351d1da4f55f
Makhmoode <sergey.belikov@webasyst.net>
2026-07-13 18:46:27 +0300

    Add plugin icon

--- lib/config/plugin.php
+++ lib/config/plugin.php
@@ -2,6 +2,7 @@
 return array(
     'name'        => /*_wp*/('S3 server'),
     'description' => /*_wp*/('Provides S3-compatible protocol access to Files app storages'),
+    'icon'        => 'img/logo.png',
     'img'         => 'img/logo.png',
     'version'     => '1.0.0',
     'vendor'      => 'webasyst',
